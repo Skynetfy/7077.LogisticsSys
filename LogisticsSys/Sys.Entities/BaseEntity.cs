bcf49f9c6a596522c8a7fd94cb945c7aa30f2e22
张志强 <skynetfyah@outlook.com>
2017-02-22 18:44:51 +0800

    sdf

---
+++ LogisticsSys/Sys.Entities/BaseEntity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arch.Data.Orm;
+
+namespace Sys.Entities
+{
+    public class BaseEntity
+    {
+        /// <summary>
+        /// </summary>
+        [Column(Name = "Id", ColumnType = DbType.Int64), ID, PK]
+        public Int64 Id { get; set; }
+
+        /// <summary>
+        /// </summary>
+        [Column(Name = "CreateDate", ColumnType = DbType.DateTime)]
+        public DateTime CreateDate { get; set; }
+    }
+}
---
+++ LogisticsSys/Sys.Entities/SysUser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Arch.Data.Orm;
+
+namespace Sys.Entities
+{
+    /// <summary>
+    /// ActivityUser
+    /// </summary>
+    [Serializable]
+    [Table(Name = "SysUser")]
+    public class SysUser: BaseEntity
+    {
+        /// <summary>
+        /// </summary>
+        [Column(Name = "UserName", ColumnType = DbType.AnsiString, Length = 50)]
+        public string UserName { get; set; }
+        /// <summary>
+        /// </summary>
+        [Column(Name = "Password", ColumnType = DbType.AnsiString, Length = 50)]
+        public string Password { get; set; }
+        /// <summary>
+        /// </summary>
+        [Column(Name = "Email", ColumnType = DbType.AnsiString, Length = 100)]
+        public string Email { get; set; }
+        /// <summary>
+        /// </summary>
+        [Column(Name = "Status", ColumnType = DbType.Int32)]
+        public int Status { get; set; }
+        /// <summary>
+        /// </summary>
+        [Column(Name = "Phone", ColumnType = DbType.AnsiString, Length = 50)]
+        public string Phone { get; set; }
+    }
+}

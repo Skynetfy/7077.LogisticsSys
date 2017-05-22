fd436b297528defae5bb403ec169c91752e07a53
skynet <skynet@DESKTOP-0RCGAKA>
2017-05-23 00:09:14 +0800

    dsfs

--- SQLQuery55.sql
+++ SQLQuery55.sql
@@ -5,6 +5,12 @@ Create table SysExchange(
   CreateDate datetime default getdate() not null,
   IsDelete bit default 0 not null
 )
-
+Create table SysDbConfig(
+  Id bigint identity(1,1) primary key not null,
+  [Key] varchar(20) default '' not null,
+  [Value] nvarchar(200) default '' not null,
+  CreateDate datetime default getdate() not null,
+  IsDelete bit default 0 not null
+)
 alter table [dbo].[SysReceiverInfo]
 add ShippingStatus int default 0 not null

9e9e3d8792a7ce89bb9073cf080462c0cc57dfcf
张志强 <zhiqiang.zhang@nttdata.com>
2020-04-17 19:00:14 +0800

    s

--- LogisticsSys/Sys.WebUI/App_Start/RouteConfig.cs
+++ LogisticsSys/Sys.WebUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Sys.WebUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Export", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
